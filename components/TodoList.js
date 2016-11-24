159f317885ba064a0406ce5eeacc8843269668c0
bachnx1 <bachxn228@gmail.com>
2016-11-24 07:06:39 +0700

    20161123

---
+++ components/TodoList.js
@@ -0,0 +1,31 @@
+/**
+  Author:"NguyenBach228"
+  20161123
+*/
+
+import React, { PropTypes } from 'react'
+import Todo from './Todo'
+
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {
+      todos.map(todo =>
+        <Todo
+          key = {todo.id}
+          {..todo}
+          oncClick = {() => onTodoClick(todo.id)}
+        />
+      )}
+  </ul>
+)
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  })).isRequired).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+}
+
+export default TodoList
